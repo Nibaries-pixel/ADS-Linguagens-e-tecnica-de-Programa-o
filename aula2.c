8a4c47bf430da9de02fa29d3132706cdcdc9f2f9
Nibaries-pixel <nicolasriello25@gmail.com>
2026-08-05 22:27:02 -0300

    Add files via upload

---
+++ aula2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	
+	double a,b,c,d,e,f;
+	
+	
+	a = 16;
+	b = 3;
+	c = 60;
+	d = (a+b+c)/3;
+
+	printf ("a media de %lf e %lf e %lf = %.9lf\n", a,b,c,d); 
+	
+	
+	return 0;
+}
