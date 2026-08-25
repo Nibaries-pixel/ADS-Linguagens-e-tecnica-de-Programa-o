26c5d1687e307332eef356d0d99c0f56cb97cc62
Nibaries-pixel <nicolasriello25@gmail.com>
2026-08-24 21:19:34 -0300

    tirei os exercicios de comentados
    tirei os /* e */

--- listaa2.c
+++ listaa2.c
@@ -4,7 +4,7 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	/*
+	
 	printf("\n------------EXERCICIO 1------------\n");
 	int idade, anoatual, ano;
 	anoatual = 2026;
@@ -15,9 +15,9 @@ int main(int argc, char *argv[]) {
 	ano = (anoatual - idade);
 	
 	printf("\nVoce nasceu em %d", ano);
-	*/
 	
-	/*
+	
+	
 	printf("\n------------EXERCICIO 2------------\n");
 	
 	float K,M;
@@ -28,9 +28,9 @@ int main(int argc, char *argv[]) {
 	M = K/36;
 	
 	printf("%.1f km/h convertido em metros e %.2f m/s",K,M);
-	*/
 	
-	/*
+	
+	
 	printf("\n------------EXERCICIO 3------------\n");
 	
 	float real,dolar,valor;
@@ -43,9 +43,9 @@ int main(int argc, char *argv[]) {
 	valor = real/dolar;
 	
 	printf("\nO valor em dolares e: $%.2f", valor);
-	*/
 	
-	/*
+	
+	
 	printf("\n------------EXERCICIO 4------------\n");
 	
 	float c,f;
@@ -56,10 +56,10 @@ int main(int argc, char *argv[]) {
 	f = (c*1.8)+32;
 	
 	printf("%.1f C em fahrenheit e %.1f F", c,f);
-	*/
 	
 	
-	/*
+	
+	
 	printf("\n------------EXERCICIO 5------------\n");
 	
 	float R,G;
@@ -70,9 +70,9 @@ int main(int argc, char *argv[]) {
 	R = G*3.141592/180;
 	
 	printf("\nO angulo em radianos e %.2f", R);
-	*/
 	
-	/*
+	
+	
 	printf("\n------------EXERCICIO 6------------\n");
 	
 	int n;
@@ -81,9 +81,9 @@ int main(int argc, char *argv[]) {
 	scanf("%d", &n);
 	
 	printf("O sucessor do numero %d e: %d e o antecessor e: %d",n,n+1,n-1);
-	*/
+	
 
-	/*
+	
 	printf("\n------------EXERCICIO 7------------\n");
 	
 	float a,b,c,valor;
@@ -91,8 +91,8 @@ int main(int argc, char *argv[]) {
 	printf("O valor do primeiro recebera: R$%.2f", a = 780000*46/100);
 	printf("\nO valor do segundo recebera: R$%.2f", b = 780000*32/100);
 	printf("\nO valor do terceiro recebera: R$%.2f", c = 780000*22/100);
-	*/
-	/*
+	
+	
 	printf(\n"------------EXERCICIO 8------------\n");
 	
 	int s,h,m,ss;
@@ -107,8 +107,8 @@ int main(int argc, char *argv[]) {
 	ss = (s%3600)%60;
 		
 	printf("Foram %d:%d:%d",h,m,ss);
-	*/
-	/*
+	
+	
 	printf("\n------------EXERCICIO 9------------\n");
 	
 	float tempo,kmh,l, km;
@@ -122,9 +122,9 @@ int main(int argc, char *argv[]) {
 	l = km/12;
 	
 	printf("Foram gastos %.3f Litros na viagem",l);
-	*/
 	
-	/*
+	
+	
 	printf("\n------------10------------\n");
 	
 	int a, b, c, maior_temp, maior;
@@ -136,7 +136,7 @@ int main(int argc, char *argv[]) {
 	maior = ((maior_temp+c)+abs(maior_temp-c))/2;
 	
 	printf("\nO maior numero entre %d %d e %d e o %d ", a, b, c, maior);
-	*/
+	
 
 	
 	
