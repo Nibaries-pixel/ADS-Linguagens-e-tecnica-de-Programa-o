6fec22f2b833bfda734c530c1a48968c389ee64f
NicolasR <nicolasriello25@gmail.com>
2026-08-31 22:23:38 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,30 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define pi 3.14159
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+/* crie um programa que leia um número, entre 1 e 9 e caso ele seja positivo, 
+ verifique se ele seja positivo, verifique se ele é um numero primo, caso seja,
+ imprima seu dobro, caso não seja primo, verifique se é par ou impar. 
+ Se for par mostre o resultado da soma com 2. Se for impar mostre o seu proximo
+ e seu anterior..
+ Caso não seja entre 1 e 9 mostre _out_of_range */
 
 int main(int argc, char *argv[]) {
 	
+	/*
+	int n;
+	printf("Entre com o numero: ");
+	scanf("%d", &n);
 	
-	float raio, area, raioq;
-	printf("Insira o valor do R? \n");
-	scanf("%f", &raio);
-	raioq = raio*raio;
-	area = pi*raioq;
-	printf("A are do circulo de raio %f = %f", raio, area);
+	 
+	if(n<10 && n>1)
 	
-	// area do trapezio descendente
+		if(n==1 || n==2 || n==3 || n==5 || n==7)
+			printf("o dobro de %d = %d", n, (n*2));
+		else if (n%2 == 0)
+			printf("%d+2 = %d",n, n+2);
+		else
+			printf("|%d|%d|%d|",n-1,n, n+1);
+		
+	else printf("out_of_range");
+	*/
 	
-	float areaT, BaseB, Baseb, altura;
+	char op;
+	scanf("%c", &op);
+	switch(op){
 	
-	printf("\nEscreve o valor da base maior: \n");
-	scanf("%f", &BaseB);
-	printf("Escreva o valorda base menor: \n");
-	scanf("%f", &Baseb);
-	printf("Escreva o valor da altura: \n");
-	scanf("%f", &altura);
+	case 'a': printf("Opa ta falando com ele"); break;
 	
-	areaT = ((BaseB + Baseb)*altura)/2;
-	printf("Area do trapezio é %f", areaT);
+	case 'b': printf("Benentido"); break;
+	
+	case 'c': printf("É complicado a situação"); break;
+	
+	case 'd': printf("Tamo junto"); break;
+	}
+	
 	return 0;
 }
